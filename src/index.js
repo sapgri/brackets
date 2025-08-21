cf4ffe3e6f5e36e62f6320498d71078ccd8f5d4c
SapGri <sapgri360@gmail.com>
2025-08-21 23:45:02 +0300

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  const map = new Map(bracketsConfig.map((pair) => [pair[1], pair[0]]));
+  const stack = [];
+
+  for (let i = 0; i < str.length; i += 1) {
+    if (map.has(str[i]) && stack.length && stack.at(-1) === map.get(str[i])) {
+      stack.pop();
+    } else if ([...map.values()].includes(str[i])) {
+      stack.push(str[i]);
+    } else {
+      return false;
+    }
+  }
+
+  return !stack.length;
 };
